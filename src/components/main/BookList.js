dd77d524b52bf66777d9a34a9f0fa5a46d329ded
Jeff C <blackone787@gmail.com>
2018-10-22 18:14:45 -0500

    Fixing Orson book url.

--- src/components/main/BookList.js
+++ src/components/main/BookList.js
@@ -17,7 +17,7 @@ export default props => {
       name: "Ender's Game",
       author: 'Orson Scott Card',
       url:
-        'http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api'
+        'http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api'
     }
   ];
   const want = [
